47d2db307a1df599245861638598c1fec2ad922b
Daniela <ddzanfir@gmail.com>
2022-08-21 16:14:50 +0300

    implemented redux-persist

--- src/index.js
+++ src/index.js
@@ -2,25 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 //import { UserProvider } from './contexts/userContext';
 //import { CartProvider } from './contexts/CartContext';
-import { store } from './store/store';
+import { store, persistor } from './store/store';
 
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        {/* <UserProvider> */}
-          {/* <CartProvider> */}
-            <App />
-          {/* </CartProvider> */}
-        {/* </UserProvider> */}
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          {/* <UserProvider> */}
+            {/* <CartProvider> */}
+              <App />
+            {/* </CartProvider> */}
+          {/* </UserProvider> */}
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,26 @@
 import { compose, legacy_createStore as createStore, applyMiddleware, legacy_createStore } from  'redux';
 import logger from 'redux-logger';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import { rootReducer } from './root-reducer';
 
 
 const middleWares = [logger];
 
+const persistConfig = {
+    key: "root", 
+    storage, 
+    
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 const composedEnhancers = compose(applyMiddleware(...middleWares))
 
-export const store = legacy_createStore(rootReducer, undefined, composedEnhancers);
+export const store = legacy_createStore(
+    persistedReducer, 
+    undefined, 
+    composedEnhancers
+    );
+
+export const persistor = persistStore(store);
